63d5c15db4f29e70fb880eaa45250a6d8d0ab82d
TiagoDeFranca <tiago.franca@sankhya.com.br>
2020-03-10 22:28:05 -0300

    adicionado # no for

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,10 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("OI Mundo!!!");
 		System.out.println("boa tarde!!!");
-
+		
+		for ( int i = 0; i < 10; i++) {
+			System.out.println("FOR TESTE");
+		}
 	}
 
 }
